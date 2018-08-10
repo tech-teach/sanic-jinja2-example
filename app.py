369bae4635f4b4e18dfd1eaa41ae333d27a3e9b2
mrodriguezv <mrodriguezv92@gmail.com>
2018-08-09 23:44:23 -0500

    the delete_task and mark_as_done functions were added

--- app.py
+++ app.py
@@ -6,28 +6,55 @@ from jinja2 import Template
 app = Sanic()
 
 txt_template = open("template.html").read()
-
 tasks = []
 
-#generador de id
-def numId():
-	i=0
-	while True:
-		yield i
-		i+=1
-
-nums=numId()
-
 @app.route('/', methods=["GET"])
-async def test(request):
-    task = request.args.get('task')
-    if task:
-        tasks.append({str(next(nums)):task})
+async def save_task(request):
+	task = request.args.get('task')
+	if task:
+		tasks.append([task,False])
 
-    template = Template(txt_template)
-    html_template = template.render(tasks=tasks)
-    return response.html(html_template)
+	template = Template(txt_template)
+	html_template = template.render(tasks=tasks)
+	return response.html(html_template)
 
+@app.route('/task/<_id>', methods=["GET"])
+async def delete_task(request,_id):
+	template = Template(txt_template)
+	respuesta={}
+	if int(_id) >= len(tasks) or int(_id) < 0:
+		respuesta={"status":"not found"}
+	try:
+		del tasks[int(_id)]
+	except ValueError:
+		respuesta={"status": "id is not valid"}
+
+	if len(respuesta) == 0:
+		respuesta={"status": "deleted", "id": _id}
+
+	html_template = template.render(tasks=tasks,message=respuesta)
+	return response.html(html_template)
+
+@app.route('/realized/<_id>', methods=["GET"])
+async def mark_as_done(request,_id):
+	template = Template(txt_template)
+	respuesta={}
+	if int(_id) >= len(tasks) or int(_id) < 0:
+		respuesta={"status":"not found"}
+	try:
+		if tasks[int(_id)][1]:
+			tasks[int(_id)][1]=False
+		else:
+			tasks[int(_id)][1]=True
+
+	except ValueError:
+		respuesta={"status": "id is not valid"}
+
+	if len(respuesta) == 0:
+		respuesta={"status": "realized", "id": _id}
+
+	html_template = template.render(tasks=tasks,message=respuesta)
+	return response.html(html_template)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=8000, debug=True)
